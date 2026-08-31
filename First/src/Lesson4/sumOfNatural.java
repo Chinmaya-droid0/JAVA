d2d7b5a8d9372a73010e375beb889ea93f17da15
Chinmaya-droid0 <chinmayacodes@yahoo.com>
2026-08-31 20:23:09 +0530

    Final commit

---
+++ First/src/Lesson4/sumOfNatural.java
@@ -0,0 +1,18 @@
+package Lesson4;
+
+import java.util.Scanner;
+public class sumOfNatural {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Range: ");
+        int n = sc.nextInt();
+
+        int sum =0;
+
+        for(int i=1; i <= n; i++){
+            sum += i;
+
+        }
+        System.out.println("Sum of Natural numbers: "+sum);
+    }
+}
