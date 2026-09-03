afefa87fc43e441001728c0375a0d1166dca4d31
Chinmaya-droid0 <chinmayacodes@yahoo.com>
2026-09-03 11:54:51 +0530

    Latest

---
+++ First/src/Lesson6/armstrong.java
@@ -0,0 +1,36 @@
+package Lesson6;
+
+import java.util.Scanner;
+public class armstrong {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+
+         if(armstrong(n)){
+             System.out.println("Yes");
+         } else{
+             System.out.println("No");
+         }
+
+
+    }
+
+    public static boolean armstrong(int n){
+       int digits = (int)Math.log10(n)+1;
+       int num =n;
+        double ans =0;
+
+        while(n!=0){
+            int last = n%10;
+            ans = ans +Math.pow(last,digits);
+            n=n/10;
+        }
+
+        if(num == ans ){
+            return true;
+        } else{
+            return false;
+        }
+
+    }
+}
