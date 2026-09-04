269ff2d223a4d8b796b8383253174ec3dba3bde6
Chinmaya-droid0 <chinmayacodes@yahoo.com>
2026-09-04 12:38:15 +0530

    Array Reverse

--- First/src/Lesson7/arrayReverse.java
+++ First/src/Lesson7/arrayReverse.java
@@ -1,4 +1,33 @@
 package Lesson7;
 
 public class arrayReverse {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        display(arr);
+        reverse(arr);
+        display(arr);
+    }
+
+    public static void display(int[] arr) {
+        for (int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void reverse(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+
+    }
 }
